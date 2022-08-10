3aa0215ef9a3691f78c0ae24fff1096573f6bcf2
Valzang <72681774+Valzang@users.noreply.github.com>
2022-08-10 17:46:10 +0900

    Update Scene.cpp

--- Scene.cpp
+++ Scene.cpp
@@ -55,7 +55,7 @@ void cScene::Render(HDC _hdc)
 	int w = m_SceneImg->GetWidth();
 	int h = m_SceneImg->GetHeight();
 	
-	//graphics.DrawImage(m_SceneImg, Rect(0,0, w, h), 0, 0, w, h, UnitPixel, &m_imgAttr);
+	graphics.DrawImage(m_SceneImg, Rect(0,0, w, h), 0, 0, w, h, UnitPixel, &m_imgAttr);
 	for (UINT i = 0; i < (UINT)GROUP_TYPE::END; ++i)
 	{
 		for (UINT j = 0; j < m_arr_obj[i].size(); ++j)
